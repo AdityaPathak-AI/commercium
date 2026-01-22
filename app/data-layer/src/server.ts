624de36cb44f16197aedf7aa69ede700b0ee9163
Aditya Pathak <sumitpathak861@gmail.com>
2026-01-23 01:19:43 +0530

    feat : api health status on interface.

--- app/data-layer/src/server.ts
+++ app/data-layer/src/server.ts
@@ -22,5 +22,5 @@ app.get("/api/health", (req: Request, res: Response) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Check api health at http://localhost:${PORT}/api/health`);
 });
